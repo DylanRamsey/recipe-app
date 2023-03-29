f113c0c461e4acef3c2d5fa9a24259442b078478
Dylan <dylanbradramsey@gmail.com>
2023-03-29 17:34:39 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import "/Users/dylanramsey/Sites/rt-recipes/src/assets/sass/main.scss"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1 className="bg-neutral-700	">this is an app</h1>
+    </div>
+  )
+}
+
+export default App
