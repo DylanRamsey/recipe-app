a9bbec1caf89546efe91eee7bf9fa59c065b0575
Dylan <dylanbradramsey@gmail.com>
2023-04-03 17:34:27 -0400

    added modal close icon and functionality for view recieps

--- src/components/organism/Modal.jsx
+++ src/components/organism/Modal.jsx
@@ -1,19 +1,35 @@
 import AddRecipe from "../molecules/AddRecipe"
 import ViewRecipe from "../molecules/ViewRecipe"
+import closeIcon from "../../assets/icons/modal-close.svg"
 function Modal({modalType, afterSetModalRecipeTitle, viewRecipeModal, setViewRecipeModal}) {
+
+  function closeModal() {
+    if(modalType === 'viewRecipe'){
+      setViewRecipeModal(false)
+    }
+  }
   return (
     <div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-    <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+      <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
       <div className="fixed inset-0 z-10 overflow-y-auto">
         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-          <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-            <div className="bg-cream px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+          <div className="relative transform rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+          <div 
+            className="absolute -right-6 -top-4 cursor-pointer"
+            onClick={closeModal}
+          >
+            <img 
+              src={closeIcon} 
+              alt="Close icon"
+            />
+          </div>
+            <div className="bg-cream rounded-lg px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
               <div className="">
                 <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                  {modalType == 'addRecipe' &&
+                  {modalType === 'addRecipe' &&
                     <AddRecipe />
                   }
-                  {modalType == 'viewRecipe' &&
+                  {modalType === 'viewRecipe' &&
                     <ViewRecipe
                       afterSetModalRecipeTitle={afterSetModalRecipeTitle}
                       viewRecipeModal={viewRecipeModal}
