9a3379699238ba3cc4d9793649338fada829a619
Dylan <dylanbradramsey@gmail.com>
2023-05-10 00:05:34 -0400

    Adding async handler

--- backend/controllers/recipeController.js
+++ backend/controllers/recipeController.js
@@ -1,31 +1,31 @@
-
+const asyncHandler = require('express-async-handler')
 // @desc GET goals
 // @route GET /api/goals
-const getRecipes = (req, res) => {
+const getRecipes = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'Get Recipes'})
-}
+})
 
 // @desc SET goals
 // @route POST /api/goals
-const setRecipe = (req, res) => {
+const setRecipe = asyncHandler(async (req, res) => {
   if(!req.body.text){
     res.status(400)
     throw new Error('Text is required')
   }
   res.status(200).json({ message: 'Set Recipe'})
-}
+})
 
 // @desc Update goals
 // @route PUT /api/goals/:id
-const updateRecipe = (req, res) => {
+const updateRecipe = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update goal ${req.params.id}`})
-}
+})
 
 // @desc Delete goals
 // @route DELETE /api/goals/:id
-const deleteRecipe = (req, res) => {
+const deleteRecipe = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete goal ${req.params.id}`})
-}
+})
 
 module.exports = {
   getRecipes,
